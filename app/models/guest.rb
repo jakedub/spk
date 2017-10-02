2701340679fc805e5b479c2ec62d6e7b8c3db22e
Christopher Nimz <cmnfv@mac.com>
2017-10-02 12:12:11 -0400

    more cat changes plus user_guest vals

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,8 @@
 class Guest < ApplicationRecord
   has_secure_token :api_token
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :address, length: { minimum: 3 }, presence: true
+  validates :city, length: { minimum: 3 }, presence: true
+  validates :state, length: { minimum: 2 }, presence: true
+  validates :postal_code, length: { minimum: 5 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_secure_token :api_token
   has_secure_password
-  validates :name, email, length: { minimum: 3 }, presence: true
-  # validates :email, length: { minimum: 3 }, presence: true
-  validates :password, length: { minimum: 10 }
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :email, length: { minimum: 3 }, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :password_digest, length: { minimum: 8 }, message: "Minimum of 8 characters"
+  validates :address, length: { minimum: 3 }, presence: true
+  validates :city, length: { minimum: 3 }, presence: true
+  validates :state, length: { minimum: 2 }, presence: true
+  validates :postal_code, length: { minimum: 5 }, presence: true
 end
