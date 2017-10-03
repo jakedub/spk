38ff29e1edd8a76d94ae6a07bd9f972b4d6290bf
jakedub <jacob.w.moore@gmail.com>
2017-10-03 18:59:16 -0400

    massive updates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,71 +5,67 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    render json: @products
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
+    @product = Product.find(params[:id])
+    render json: @products
   end
 
   # GET /products/new
-  def new
-    @product = Product.new
-  end
+  # def new
+  #   @product = Product.new
+  # end
 
   # GET /products/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /products
   # POST /products.json
-  def create
-    @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def create
+  #   @product = Product.new(product_params)
+  #
+  #   respond_to do |format|
+  #     if @product.save
+  #       format.html { redirect_to @product, notice: 'Product was successfully created.' }
+  #       format.json { render :show, status: :created, location: @product }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @product.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
-  def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @product.update(product_params)
+  #       format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @product }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @product.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /products/1
   # DELETE /products/1.json
-  def destroy
-    @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @product.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
-  # NOTE: need to be able to deduct the quantity purchased against the quantity of the product
 
-  def purchase
-    @product = Product.find(params[:id])
-    @carts.toggle(:purchase)
-    if @product.purchase
-      @product.quantity - @cart.quantity
-    end
-  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
