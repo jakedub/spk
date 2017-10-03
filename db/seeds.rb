1134963648f9aca3c9e5e7b727030d00dd80f65d
Christopher Nimz <cmnfv@mac.com>
2017-10-03 10:35:50 -0400

    seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'IronGloryInventory.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  @product = row.to_hash
+  t = Product.new
+  t.product_name = row['Product']
+  t.sku = row['SKU']
+  t.price = row['Price']
+  t.year = row['Year']
+  t.quantity = row['Available'].gsub(/\:|\D/, '')
+  t.size = row['Available'].gsub(/\:|\d/, '')
+  t.category_name = row['Category']
+  t.description = row['Description']
+  t.save
+end
