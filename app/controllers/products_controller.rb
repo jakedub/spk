aa9c730062cd1894c4cc40b4323a1e77b65df597
Christopher Nimz <cmnfv@mac.com>
2017-10-06 00:28:44 -0400

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -74,6 +74,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:product_name, :sku, :price, :year, :availabilty, :quantity, :size, :image)
+      params.require(:product).permit(:product_name, :sku, :price, :year, :availabilty, :quantity, :size, :patch)
     end
 end
